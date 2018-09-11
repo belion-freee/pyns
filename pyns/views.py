87b03f796c98f61979208459d094d29bb677ddb5
takuma_bishan.abewadana <takuma_bishan.abewadana@supership.jp>
2018-09-11 20:05:44 +0900

    [Modified] dont chache

--- pyns/views.py
+++ pyns/views.py
@@ -1,6 +1,7 @@
 from flask import request, redirect, url_for, render_template, flash
 from pyns import app, db
 from pyns.models import Entry
+import os
 
 @app.route('/')
 def index():
@@ -17,3 +18,16 @@ def add():
     db.session.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('index'))
+
+# css reload at change
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
